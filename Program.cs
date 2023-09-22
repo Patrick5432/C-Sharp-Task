b5d31df76a16d75f2290b56556996eccd356cb96
Patrick5432 <112853910+Patrick5432@users.noreply.github.com>
2023-09-22 20:29:45 +0500

    Вывод первого массива

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 4;
+int b = 8;
+
+int[] arr = new int[9];
+int j = 1;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = a * j;
+    Console.Write(arr[i] + " ");
+    j++;
+}
